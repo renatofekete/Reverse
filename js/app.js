e6777b9d197eb8e55d78c7491bc509cf663a8dbe
Renato <renatofekete.91@gmail.com>
2018-03-15 23:51:12 +0100

    Postavljene osnovne funkcije

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
-console.log("proba");
+var btn = document.getElementById("btn");
+var input = document.getElementById("submit");
+var answer = document.querySelector('.reversed p');
+var container = document.querySelector('.reversed');
+
+btn.addEventListener('click', function () {
+  answer.innerHTML = '';
+
+  var reversed = input.value.split('').reverse('');
+
+  for (i = 0; i < reversed.length; i++) {
+    (function (i) {
+      setTimeout(function () {
+        answer.innerHTML += reversed[i];
+      }, 100 * i);
+    })(i);
+
+
+  }
+
+
+
+});
+
+input.addEventListener('focus', function () {
+  input.value = '';
+
+});
+console.log("test");
